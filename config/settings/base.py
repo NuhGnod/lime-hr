bf51fdd968086f3f51e5379e1f02086205ba81fd
jisong <jisong@euclidsoft.co.kr>
2023-01-18 13:59:04 +0900

    [추가]회원 로그인 로그아웃기능 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 import environ
-
+from django.urls import reverse_lazy
 # env 파일 선언
 env = environ.Env(DEBUG=(bool, False))
 
@@ -17,6 +17,12 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
+
+LOGIN_URL = reverse_lazy('accounts:login')
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+
+SITE_ID=1
 # Application definition
 
 INSTALLED_APPS = [
--- config/views.py
+++ config/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.shortcuts import render
@@ -5,6 +6,7 @@ from django.shortcuts import render
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
+@login_required
 def home_view(request):
 
     return render(request, 'main.html')
