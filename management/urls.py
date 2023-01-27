e0d688f55e317a0aacc611a135894b3adfa8046a
kimdonghun <zhfptm12@gmail.com>
2023-01-27 10:15:47 +0900

    [추가] managemnet url 추가

--- management/urls.py
+++ management/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, re_path
-from management.views import get_all_code
+from management.views import *
 
 app_name = 'management'
 
-
 urlpatterns = [
-    path('code/', get_all_code, name='code_list'),
+    # path('code/', get_all_code, name='code_list'),
+    path('code/', code, name='code'),
+    # path('code/', ddlCode, name='modify_code')
+    path('code/list', code_list, name='code_list'),
+    path('code/add', code_add, name='code_add'),
+
+    # path('code/button')
+    # path('code/<String:pk>', code, name='code'),
 ]
