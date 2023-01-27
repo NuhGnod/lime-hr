02a5761093de905387d487fc32f1a8e4f9062cbb
jisong <jisong@euclidsoft.co.kr>
2023-01-27 14:24:05 +0900

    [추가]모델정보 수정

--- mdm/models.py
+++ mdm/models.py
@@ -56,6 +56,21 @@ class EvalSheet(models.Model):
         db_table = 'eval_sheet'
 
 
+class EvalRel(models.Model):
+    eval_rel_no = models.AutoField(primary_key=True)
+    eval_trgt_clss = models.CharField(max_length=10, blank=True, null=True)
+    eval_mem_no = models.IntegerField(null=False, blank=False, db_column='eval_mem_no')
+    be_eval_mem_no = models.IntegerField(null=False, blank=False, db_column='be_eval_mem_no')
+    reg_mem_no = models.IntegerField(blank=True, null=True)
+    modf_mem_no = models.IntegerField(blank=True, null=True)
+    reg_dt = models.DateTimeField(blank=True, null=True)
+    modf_dt = models.DateTimeField(blank=True, null=True)
+    del_yn = models.CharField(max_length=1, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'eval_rel'
+
 class AbltEvalQues(models.Model):
     eval_sheet_no = models.OneToOneField('EvalSheet', models.DO_NOTHING, db_column='eval_sheet_no', primary_key=True)
     ablt_ques_no = models.ForeignKey('AbltQuesPool', models.DO_NOTHING, db_column='ablt_ques_no')
@@ -94,9 +109,9 @@ class AbltQuesPool(models.Model):
 
 class AbltEvalRslt(models.Model):
     eval_rel_no = models.OneToOneField('EvalRel', models.DO_NOTHING, db_column='eval_rel_no', primary_key=True)
-    eval_sheet_no = models.ForeignKey(AbltEvalQues, models.DO_NOTHING, db_column='eval_sheet_no')
-    ablt_ques_no = models.ForeignKey(AbltEvalQues, models.DO_NOTHING, db_column='ablt_ques_no')
-    eval_trgt_clss = models.ForeignKey(AbltEvalQues, models.DO_NOTHING, db_column='eval_trgt_clss')
+    eval_sheet_no = models.ForeignKey('EvalSheet', models.DO_NOTHING, db_column='eval_sheet_no')
+    ablt_ques_no = models.ForeignKey('AbltEvalQues', models.DO_NOTHING, db_column='ablt_ques_no')
+    eval_trgt_clss = models.CharField(max_length=10, db_column='eval_trgt_clss')
     eval_plan_no = models.ForeignKey('EvalPlan', models.DO_NOTHING, db_column='eval_plan_no')
     ablt_eval_rslt = models.CharField(max_length=500, blank=True, null=True)
     eval_dt = models.CharField(max_length=8, blank=True, null=True)
