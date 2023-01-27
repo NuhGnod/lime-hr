3333c8a855d69debb6ae5c1148e01e49c02db21d
kimdonghun <zhfptm12@gmail.com>
2023-01-27 10:24:20 +0900

    [수정] import 추가

--- management/urls.py
+++ management/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path, re_path
-from management.views.pjt import get_all_pjt
 
+from management.views import code, code_list, code_add
+from management.views.pjt import get_all_pjt
 app_name = 'management'
 
 urlpatterns = [
     path('pjt/', get_all_pjt, name='pjt_list'),
-    # path('code/', get_all_code, name='code_list'),
     path('code/', code, name='code'),
-    # path('code/', ddlCode, name='modify_code')
     path('code/list', code_list, name='code_list'),
     path('code/add', code_add, name='code_add'),
 
-    # path('code/button')
-    # path('code/<String:pk>', code, name='code'),
 ]
