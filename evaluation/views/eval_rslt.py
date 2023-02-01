85e2f3197fe8c27275e4e16d258ba4a205334831
jisong <jisong@euclidsoft.co.kr>
2023-02-01 14:04:18 +0900

    [추가]엑셀다운로드 기능 추가

--- evaluation/views/eval_rslt.py
+++ evaluation/views/eval_rslt.py
@@ -64,21 +64,25 @@ def calc_eval_rslt(eval_plan_no):
 
     for result in result_list:
         if result['CC009004'] is not None:
-            tot_cc009004_sum += float(result['CC009004'])
+            result['CC009004'] = float(result['CC009004'])
+            tot_cc009004_sum += result['CC009004']
             tot_cc009004_cnt += 1
 
         if result['CC009003'] is not None and result['CC009002'] is not None and result['CC009001'] is not None:
             # 부하평가 비중
-            m_point = float(result['CC009001']) * (eval_plan_qs.m_eval_wght / 100)
-            tot_cc009001_sum += float(result['CC009001'])
+            result['CC009001'] = float(result['CC009001'])
+            m_point = result['CC009001'] * (eval_plan_qs.m_eval_wght / 100)
+            tot_cc009001_sum += result['CC009001']
             tot_cc009001_cnt += 1
             # 상사평가 비중
-            s_point = float(result['CC009002']) * (eval_plan_qs.s_eval_wght / 100)
-            tot_cc009002_sum += float(result['CC009002'])
+            result['CC009002'] = float(result['CC009002'])
+            s_point = result['CC009002'] * (eval_plan_qs.s_eval_wght / 100)
+            tot_cc009002_sum += result['CC009002']
             tot_cc009002_cnt += 1
             # 동료평가 비중
-            j_point = float(result['CC009003']) * (eval_plan_qs.j_eval_wght / 100)
-            tot_cc009003_sum += float(result['CC009003'])
+            result['CC009003'] = float(result['CC009003'])
+            j_point = result['CC009003'] * (eval_plan_qs.j_eval_wght / 100)
+            tot_cc009003_sum += result['CC009003']
             tot_cc009003_cnt += 1
 
             tot_rslt = s_point + j_point + m_point
@@ -90,24 +94,27 @@ def calc_eval_rslt(eval_plan_no):
             tot_sum = 0
             tot_avg_cnt = 0
             if result['CC009001'] is not None:
-                tot_sum += float(result['CC009001'])
+                result['CC009001'] = float(result['CC009001'])
+                tot_sum += result['CC009001']
                 tot_avg_cnt += 1
 
-                tot_cc009001_sum += float(result['CC009001'])
+                tot_cc009001_sum += result['CC009001']
                 tot_cc009001_cnt += 1
 
             if result['CC009002'] is not None:
-                tot_sum += float(result['CC009002'])
+                result['CC009002'] = float(result['CC009002'])
+                tot_sum += result['CC009002']
                 tot_avg_cnt += 1
 
-                tot_cc009002_sum += float(result['CC009002'])
+                tot_cc009002_sum += result['CC009002']
                 tot_cc009002_cnt += 1
 
             if result['CC009003'] is not None:
-                tot_sum += float(result['CC009003'])
+                result['CC009003'] = float(result['CC009003'])
+                tot_sum += result['CC009003']
                 tot_avg_cnt += 1
 
-                tot_cc009003_sum += float(result['CC009003'])
+                tot_cc009003_sum += result['CC009003']
                 tot_cc009003_cnt += 1
             tot_rslt = None
             try:
@@ -123,18 +130,34 @@ def calc_eval_rslt(eval_plan_no):
     avg_cc009003 = None
     avg_cc009004 = None
     avg_tot_rslt = None
+
+    # 평균 20% 미만
+    fall_avg_cc009001 = None
+    fall_avg_cc009002 = None
+    fall_avg_cc009003 = None
+    fall_avg_cc009004 = None
+    fall_avg_tot_rslt = None
     try:
         avg_cc009001 = round(tot_cc009001_sum / tot_cc009001_cnt, 2)
         avg_cc009002 = round(tot_cc009002_sum / tot_cc009002_cnt, 2)
         avg_cc009003 = round(tot_cc009003_sum / tot_cc009003_cnt, 2)
         avg_cc009004 = round(tot_cc009004_sum / tot_cc009004_cnt, 2)
         avg_tot_rslt = round(tot_rslt_sum / tot_rslt_cnt, 2)
-        eval_rslt_result.update(avg_cc009001=avg_cc009001, avg_cc009002=avg_cc009002, avg_cc009003=avg_cc009003,
-                                avg_cc009004=avg_cc009004, avg_tot_rslt=avg_tot_rslt)
+
+        fall_avg_cc009001 = avg_cc009001 - (avg_cc009001 * 0.2)
+        fall_avg_cc009002 = avg_cc009002 - (avg_cc009002 * 0.2)
+        fall_avg_cc009003 = avg_cc009003 - (avg_cc009003 * 0.2)
+        fall_avg_cc009004 = avg_cc009004 - (avg_cc009004 * 0.2)
+        fall_avg_tot_rslt = avg_tot_rslt - (avg_tot_rslt * 0.2)
+
     except ZeroDivisionError:
         pass
     eval_rslt_result.update(avg_cc009001=avg_cc009001, avg_cc009002=avg_cc009002, avg_cc009003=avg_cc009003,
-                            avg_cc009004=avg_cc009004, avg_tot_rslt=avg_tot_rslt)
+                            avg_cc009004=avg_cc009004, avg_tot_rslt=avg_tot_rslt,
+                            fall_avg_cc009001=fall_avg_cc009001,
+                            fall_avg_cc009002=fall_avg_cc009002, fall_avg_cc009003=fall_avg_cc009003,
+                            fall_avg_cc009004=fall_avg_cc009004, fall_avg_tot_rslt=fall_avg_tot_rslt
+                            )
 
     eval_rslt_result.update(eval_rslt_list=result_list)
     eval_rslt_result.update(eval_plan_detail=eval_plan_qs)
@@ -165,7 +188,6 @@ def excel_download(request):
     # 엑셀 스타일: 첫번째 열(=title)과 나머지 열(=data) 구분 위한 설정
     title_style = xlwt.easyxf(
         'pattern: pattern solid, fore_color indigo; align: horizontal center; font: color_index white;')
-    data_style = xlwt.easyxf('align: horizontal right')
 
     # 첫번째 열에 들어갈 컬럼명 설정
     col_names = ['부서', '직급', '이름', '자기평가', '상사평가', '동료평가', '부하평가', '총평점']
@@ -188,7 +210,25 @@ def excel_download(request):
     # 두번째 이후 열: 설정한 컬럼명에 맞춘 데이터 순서대로 스타일 적용하여 생성
     for row in rows:
         row_num += 1
+
         for col_num, attr in enumerate(row):
+            data_style = xlwt.easyxf('align: horizontal right;')
+            if col_num == 3:
+                if cal_result_dict['fall_avg_cc009004'] is not None and attr is not None and cal_result_dict['fall_avg_cc009004'] > attr:
+                    data_style = xlwt.easyxf('pattern: pattern solid, fore_color red; align: horizontal right;')
+            elif col_num == 4:
+                if cal_result_dict['fall_avg_cc009002'] is not None and attr is not None and cal_result_dict['fall_avg_cc009002'] > attr:
+                    data_style = xlwt.easyxf('pattern: pattern solid, fore_color red; align: horizontal right;')
+            elif col_num == 5:
+                if cal_result_dict['fall_avg_cc009003'] is not None and attr is not None and cal_result_dict['fall_avg_cc009003'] > attr:
+                    data_style = xlwt.easyxf('pattern: pattern solid, fore_color red; align: horizontal right;')
+            elif col_num == 6:
+                if cal_result_dict['fall_avg_cc009001'] is not None and attr is not None and cal_result_dict['fall_avg_cc009001'] > attr:
+                    data_style = xlwt.easyxf('pattern: pattern solid, fore_color red; align: horizontal right;')
+            elif col_num == 7:
+                if cal_result_dict['fall_avg_tot_rslt'] is not None and attr is not None and cal_result_dict['fall_avg_tot_rslt'] > attr:
+                    data_style = xlwt.easyxf('pattern: pattern solid, fore_color red; align: horizontal right;')
+
             ws.write(row_num, col_num, attr, data_style)
 
     wb.save(response)
