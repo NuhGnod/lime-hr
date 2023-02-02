6e5c6e1924e494d19485cd5a22051b38b646242b
euclid <jisong@euclidsoft.co.kr>
2023-02-02 04:12:55 +0000

    [추가]nginx 설정 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': env.db_url(
         'DEV_DATABASE_URL',
-        default='mysql://root:1q2w3e4r@localhost:3306/lime_hr_dev'
+        default='mysql://root:1q2w3e4r@220.81.201.247:24401/lime_hr_dev'
     ),
 }
 
@@ -16,18 +16,18 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'console': {
-            'level': 'INFO',
+            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
         },
     },
     'loggers': {
         'django.db.backends': {
             'handlers': ['console'],
-            'level': 'INFO',
+            'level': 'DEBUG',
         },
         'cola': {
             'handlers': ['console'],
-            'level': 'INFO'
+            'level': 'DEBUG'
         }
     },
 }
