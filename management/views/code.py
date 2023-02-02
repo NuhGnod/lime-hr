318f384adaf4aa07883c4b3280774da1ab37fad5
kimdonghun <zhfptm12@gmail.com>
2023-02-02 11:04:38 +0900

    [수정] 공통코드 생성,변경시 등록자 no, 변경자 no 추가되도록 수정

--- management/views/code.py
+++ management/views/code.py
@@ -7,24 +7,26 @@ from rest_framework.response import Response
 from django.urls import reverse
 from management.serializers import *
 
+
 @api_view(['POST', 'GET', 'DELETE'])
 @permission_classes([AllowAny])
 @csrf_exempt
 def code(req):
     test = req.data.copy()
-    test['update_user'] = req.user.id
+    test['reg_mem_no'] = req.user.id
     if req.method == 'GET':
         codes = CommCd.objects.all()
         # codes = ABCD.objects.filter(del_yn='N')
         serializer = CommCdSerializer(codes, many=True)
         listt = {
-            'comm_cd':"",
+            'comm_cd': "",
         }
-        return render(req, 'code/common_code.html', {'code_list': serializer.data, 'list': listt })
+        return render(req, 'code/common_code.html', {'code_list': serializer.data, 'list': listt})
     if req.method == 'DELETE':
         try:
             old_id = test['comm_cd']
             print(test)
+            test['modf_mem_no'] = req.user.id
             obj = CommCd.objects.get(comm_cd=old_id)
             serializer = CommCdSerializer(obj, data=test, partial=True)
             serializer.is_valid()
@@ -37,14 +39,18 @@ def code(req):
     if req.method == 'POST':
         try:
             old_id = test['comm_cd']
-            obj = CommCd.objects.get(comm_cd=old_id) # 존재하지않으면 doesnotexist 예외처리 -> except구문
-            #obj가 존재하는 경우 == 이미 있는 코드 => 수정 로직
+            test['modf_mem_no'] = req.user.id
+
+            obj = CommCd.objects.get(comm_cd=old_id)  # 존재하지않으면 doesnotexist 예외처리 -> except구문
+            # obj가 존재하는 경우 == 이미 있는 코드 => 수정 로직
             serializer = CommCdSerializer(obj, data=test, partial=True)
             serializer.is_valid()
             serializer.save()
             return redirect(reverse('management:code'), req)
         except:
-            serializer = CommCdSerializer(data=req.data, partial=True)
+            test['modf_mem_no'] = req.user.id
+
+            serializer = CommCdSerializer(data=test, partial=True)
             serializer.is_valid()
             serializer.save()
             return redirect(reverse('management:code'), req)
@@ -57,7 +63,8 @@ def code_list(req):
     comm_cd = req.GET['comm_cd']
     data = CommCd.objects.filter(comm_cd=comm_cd)
     serializer = CommCdSerializer(data, many=True)
-    return render(req, 'code/code_put.html', {'list': serializer.data,})
+    return render(req, 'code/code_put.html', {'list': serializer.data, })
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
